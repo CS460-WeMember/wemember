75caa191e83323071ed5aba50666dcdee45afdbb
yltan <yaolongt@gmail.com>
2023-03-21 18:26:22 +0800

    audio works

--- src/components/ReminderCard.jsx
+++ src/components/ReminderCard.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Batch from "./Batch.jsx";
 
+const soundLevel = {
+  off: 0,
+  low: 0.3,
+  mid: 0.6,
+  high: 1,
+}
+
 const ReminderCard = ({ item, backEndUrl }) => {
   function getImageUrl(item) {
     if (!item.picture) {
@@ -9,8 +16,42 @@ const ReminderCard = ({ item, backEndUrl }) => {
       return backEndUrl + "/api/files/"+ item["@collectionName"] + "/" + item.id + "/" + item.picture;
     }
   }
-  // console.log(item["@collectionName"]);
-  
+
+  function getAudioUrl(item) {
+    if (!item.audio) {
+      return null;
+    } else {
+      return backEndUrl + "/api/files/"+ item["@collectionName"] + "/" + item.id + "/" + item.audio;
+    }
+  }
+
+  const [audio, setAudio] = useState(new Audio(getAudioUrl(item)));
+
+  console.log(item.options.sound);
+
+  useEffect(() => {
+    if (audio) {
+      if (item.options.sound === "off") {
+        audio.volume = soundLevel.off
+      } else if (item.options.sound === "low") {
+        audio.volume = soundLevel.low
+      } else if (item.options.sound === "mid") {
+        audio.volume = soundLevel.mid
+      } else {
+        audio.volume = soundLevel.high
+      }
+      setAudio(audio);
+    }
+    document.addEventListener("click", playAudio);
+    return () => {
+      document.removeEventListener("click", playAudio);
+    };
+  }, []);
+
+  const playAudio = () => {
+    audio.play();
+  };
+
   const image = (item) => {
     const itemPicture = getImageUrl(item);
     if (itemPicture == null) {
