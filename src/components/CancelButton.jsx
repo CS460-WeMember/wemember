6caaf6987641d850da5150d255d86c08ca097f45
zi hui <zihuic02@gmail.com>
2023-03-25 16:55:55 +0800

    revert try catch

--- src/components/CancelButton.jsx
+++ src/components/CancelButton.jsx
@@ -1,45 +1,40 @@
-import React, { Component } from 'react';
-import {ImCross} from "react-icons/im";
+import React, { Component } from "react";
+import { ImCross } from "react-icons/im";
 import "../styles/CancelButton.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import PocketBase from "pocketbase";
 import url from "../global/PocketbaseURL";
 
+function CancelButton({ style, repeat, recordId }) {
+  repeat = localStorage.getItem("repeat");
+  recordId = localStorage.getItem("recordId");
+  const navigate = useNavigate();
 
-function CancelButton({style, repeat, recordId}) {
-    repeat = localStorage.getItem("repeat");
-    recordId = localStorage.getItem("recordId");
-    const navigate = useNavigate();
+  console.log("recordId: " + recordId);
 
-    console.log("recordId: " + recordId);
-
-    async function handleClick(event) {
-        try {
-            if (!(recordId ==="null")) {
-                var collection = "";
-                if (repeat === "nil") {
-                    collection = "adhoc";
-                } else {
-                    collection = "regular"
-                }
-                const pb = new PocketBase(url);
-                const response = await pb.collection(collection).delete(recordId);
-                localStorage.setItem("repeat", null);
-                localStorage.setItem("recordId", null);
-            }
-            navigate("/");
-        } catch (error) {
-            navigate("/");
-        }
+  async function handleClick(event) {
+    if (!(recordId === "null")) {
+      var collection = "";
+      if (repeat === "nil") {
+        collection = "adhoc";
+      } else {
+        collection = "regular";
+      }
+      const pb = new PocketBase(url);
+      const response = await pb.collection(collection).delete(recordId);
+      localStorage.setItem("repeat", null);
+      localStorage.setItem("recordId", null);
     }
+    navigate("/");
+  }
 
-    return(
-        <button style={style} onClick={handleClick}>
-            <div className='circle' >
-                <ImCross className='cross' ></ImCross>
-            </div>
-        </button>
-    )
+  return (
+    <button style={style} onClick={handleClick}>
+      <div className="circle">
+        <ImCross className="cross"></ImCross>
+      </div>
+    </button>
+  );
 }
 
 export default CancelButton;
